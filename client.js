8a5f1f191c3d0c891b7fb0e9f73b335845e127c3
MetaStag <thegreek132@gmail.com>
2022-06-01 11:28:25 +0530

    search functionality

--- client.js
+++ client.js
@@ -2,23 +2,38 @@
 // https://www.geeksforgeeks.org/working-with-apis-in-javascript/
 // https://en.wikipedia.org/wiki/Special:ApiSandbox#action=parse&format=json&page=Perseus
 
-function apiCall() {
-    const url = `https://en.wikipedia.org/w/api.php?action=parse&format=json&origin=*&page=${document.getElementById('page').value}`;
-    fetch(url).then(r=> r.json()).then(data=> { display(data)});
+function page() {
+    const request = `https://en.wikipedia.org/w/api.php?action=parse&format=json&origin=*&page=${document.getElementById('page').value}`;
+    fetch(request).then(r=> r.json()).then(data=> { displayPage(data)});
 }
 
-function display(data) {
-    document.getElementById("heading").innerHTML = data.parse.title;
-    //console.log(data.parse.text['*']);
-    document.getElementById("content").innerHTML = data.parse.text['*'];
+function search(query) {
+    const request = `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&prop=utf8=1&origin=*&&srinfo=&srprop=snippet&srsearch=${query}`;
+    fetch(request).then(r=> r.json()).then(data=> { displaySearch(data)});
 }
 
-// Enter key event for box
+function displaySearch(data) {
+    let display = '';
+    for (let i = 0; i < 10; i++){
+        display += `<h3>${data.query.search[i].title}</h3>`;
+        display += `<p>${data.query.search[i].snippet}</p>`;
+    }
+    document.getElementById('results').innerHTML = display;
+
+}
+
+function displayPage(data) {
+    document.getElementById("page").innerHTML = `<h1>${data.parse.title}</h1>`;
+    document.getElementById("content").innerHTML = `<p>${data.parse.text['*']}</p>`;
+}
+
+
+// Enter key event for search box
 window.onload = function() {
-    document.getElementById("page").addEventListener("keypress", function(event) {
+    document.getElementById('search').addEventListener("keypress", function(event) {
         if (event.key === "Enter") {
             event.preventDefault();
-            apiCall();
+            search(document.getElementById('search').value);
     }
     })
 }
